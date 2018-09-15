e7087482fae042dd3db2e54df9de4c652f7a4d88
Frederik Rehbach <frederik.re@gmail.com>
2018-09-15 15:28:18 +0200

    Add tests tutorials

---
+++ tests/testthat/test_mainFunctions.R
@@ -0,0 +1,17 @@
+context("Testing Main Functions")
+
+test_that("Tutorials work correctly", {
+    currentlyExistingTutorials <- c(1)
+
+    expect_equal(length(ideaTutorial()), length(currentlyExistingTutorials))
+    for(e in ideaTutorial()){
+        expect_true(check_string(e))
+    }
+
+    expect_error(ideaTutorial(-1), regexp = "tutorial")
+    expect_error(ideaTutorial(0), regexp = "tutorial")
+    expect_error(ideaTutorial(7), regexp = "tutorial")
+    expect_error(ideaTutorial(length(currentlyExistingTutorials)+1), regexp = "tutorial")
+    expect_error(ideaTutorial(NA), regexp = "Index was not numeric")
+    expect_error(ideaTutorial("Hi"), regexp = "Index was not numeric")
+})
