1d059f65ed3b758600f491f23100ddf0bd62d73a
Frederik Rehbach <frederik.re@gmail.com>
2018-09-17 09:51:44 +0200

    Fix ggplot2 requirement bug

--- R/anyTimePlot.R
+++ R/anyTimePlot.R
@@ -47,6 +47,7 @@ generateResultDF <- function(resultList){
 #' @export
 anyTimePerformancePlot <- function(data, yLim = NULL, xLim = NULL, ylog = F
                                    , xlog = F, useMinMax = T, confidenceInterval = 0.95){
+    requireNamespace("ggplot2")
     dfNames <- c("algoName", "replication", "iteration", "iterBest")
     if(!all(dfNames %in% names(data))){
         stop("Wrong df names were provided")
@@ -85,8 +86,8 @@ anyTimePerformancePlot <- function(data, yLim = NULL, xLim = NULL, ylog = F
     plotDF$min <- as.numeric(levels(plotDF$min))[plotDF$min]
     plotDF$max <- as.numeric(levels(plotDF$max))[plotDF$max]
 
-    h <- ggplot2::ggplot(plotDF, aes(x = iteration, y = mean, color = algoName)) + ggplot2::geom_line()
-    h <- h + ggplot2::geom_ribbon(aes(ymin = min, ymax = max, fill = algoName), alpha = 0.2, show.legend = F)
+    h <- ggplot2::ggplot(plotDF, ggplot2::aes(x = iteration, y = mean, color = algoName)) + ggplot2::geom_line()
+    h <- h + ggplot2::geom_ribbon(ggplot2::aes(ymin = min, ymax = max, fill = algoName), alpha = 0.2, show.legend = F)
     if(ylog){
         h <- h + ggplot2::scale_y_log10()
     }
